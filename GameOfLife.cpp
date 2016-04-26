ee14c915802aedeba524b1fcbc91ec2e1c668c47
bgeri74 <bgeri74@gmail.com>
2016-04-26 21:37:02 +0200

    Licence update

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -11,6 +11,7 @@
  * Copyright (C) 2016 Gergő Bogacsovics, bgeri74@gmail.com
  * 
  * 
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
--- GameOfLife.h
+++ GameOfLife.h
@@ -11,7 +11,8 @@
  * @section LICENSE
  *
  * Copyright (C) 2015, 2016 Norbert Bátfai, batfai.norbert@inf.unideb.hu, nbatfai@gmail.com
- *
+ * Copyright (C) 2016 Gergő Bogacsovics, bgeri74@gmail.com
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
